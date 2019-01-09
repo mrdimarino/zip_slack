bf18f14bb3c73a3ba498df120ee60fdd63eacf37
Michelle DiMarino <mdimarino.md@gmail.com>
2019-01-09 15:33:01 -0500

    receiving messages

--- src/main/resources/static/main.js
+++ src/main/resources/static/main.js
@@ -5,16 +5,12 @@ var chatPage = document.querySelector('#slackApp');
 var usernameForm = document.querySelector('#usernameForm');
 var messageForm = document.querySelector('.message-box');
 var messageInput = document.querySelector('#message-input');
-var messageArea = document.querySelector('.messageDisplay');
+var messageArea = document.querySelector('#messageDisplay');
 var connectingElement = document.querySelector('.connecting');
 
 var stompClient = null;
 var username = null;
 
-var colors = [
-    '#2196F3', '#32c787', '#00BCD4', '#ff5652',
-    '#ffc107', '#ff85af', '#FF9800', '#39bbb0'
-];
 
 function connect(event) {
     username = document.querySelector('#name').value.trim();
@@ -33,10 +29,8 @@ function connect(event) {
 
 
 function onConnected() {
-    // Subscribe to the Public Topic
     stompClient.subscribe('/topic/public', onMessageReceived);
 
-    // Tell your username to the server
     stompClient.send("/app/chat.addUser",
         {},
         JSON.stringify({sender: username, type: 'JOIN'})
@@ -77,19 +71,12 @@ function onMessageReceived(payload) {
         message.content = message.sender + ' joined!';
     } else if (message.type === 'LEAVE') {
         messageElement.classList.add('event-message');
-        message.content = message.sender + ' left!';
+        message.content = message.sender + ' has left the building!';
     } else {
         messageElement.classList.add('chat-message');
 
-        var avatarElement = document.createElement('i');
-        var avatarText = document.createTextNode(message.sender[0]);
-        avatarElement.appendChild(avatarText);
-        avatarElement.style['background-color'] = getAvatarColor(message.sender);
-
-        messageElement.appendChild(avatarElement);
-
         var usernameElement = document.createElement('span');
-        var usernameText = document.createTextNode(message.sender);
+        var usernameText = document.createTextNode(message.sender + ': ');
         usernameElement.appendChild(usernameText);
         messageElement.appendChild(usernameElement);
     }
@@ -97,6 +84,7 @@ function onMessageReceived(payload) {
     var textElement = document.createElement('p');
     var messageText = document.createTextNode(message.content);
     textElement.appendChild(messageText);
+    messageElement.appendChild(usernameElement);
 
     messageElement.appendChild(textElement);
 
@@ -104,15 +92,6 @@ function onMessageReceived(payload) {
     messageArea.scrollTop = messageArea.scrollHeight;
 }
 
-
-function getAvatarColor(messageSender) {
-    var hash = 0;
-    for (var i = 0; i < messageSender.length; i++) {
-        hash = 31 * hash + messageSender.charCodeAt(i);
-    }
-    var index = Math.abs(hash % colors.length);
-    return colors[index];
-}
 
 usernameForm.addEventListener('submit', connect, true);
 messageForm.addEventListener('submit', sendMessage, true);
